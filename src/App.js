4a677a7a12116ea19f4655d9e1aeb9e7aa878652
tehyalenka <131363300+tehyalenka@users.noreply.github.com>
2023-10-21 21:40:33 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import React from "react";
+
+import UserList from "./components/UserList";
+
+function App() {
+  return (
+    <>
+      {/* REACT TASK-4*/}
+      <div className="App">
+        <UserList />
+      </div>
+    </>
+  );
+}
+
+export default App;
